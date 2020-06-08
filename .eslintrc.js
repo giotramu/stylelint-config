2f9072d4fc1f6f3b01faf98799a8bce68c2ee5c9
giotramu <14157578+giotramu@users.noreply.github.com>
2020-06-08 14:24:23 +0200

    Upgrade eslint-config-contactlab to v5.x and fix minor stuffs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: ['contactlab/typescript', 'prettier']
+
+  extends: ['contactlab/typescript', 'prettier/@typescript-eslint'],
+
+  env: {
+    jest: true
+  },
+
+  rules: {
+    // --- TypeScript
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          '{}': false
+        },
+        extendDefaults: true
+      }
+    ]
+  }
 };
